db9e7b1959e252e16c65cc293f50c6703ff1e92f
ZIZO17z <zes79586@gmail.com>
2026-04-09 01:50:49 +0200

    I have updated some errors and some conflicts in the code

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pygame
 
 board = [
     [0,0,0,0,0,0,0,0,0],
@@ -12,14 +11,14 @@ board = [
     [0,0,0,0,0,0,0,0,0],   
 ]
 
-def board_setup(board):
+
+
+
+def find_position(board):
     for row in range(len(board)):
         for column in range(len(board[0])):
-            print(board[row][column], end=" ")
-            
-        print()
-        if row % 3 == 0:
-            print("_" * 18)
-
-
-board_setup(board)
+            position = (row,column)
+            return position
+        
+def valid():
+    pass
